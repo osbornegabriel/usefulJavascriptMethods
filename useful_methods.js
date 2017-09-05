20613a1877438efd2bab8d010e8684a5b32bc57e
Gabriel Osborne <osbornegabriel@gmail.com>
2017-09-05 16:00:00 -0500

    Add pop function to String

--- useful_methods.js
+++ useful_methods.js
@@ -1,6 +1,17 @@
-// Array prototype methods
+// ****Array prototype methods****
 
   // #Sample : Returns a random value from the array's collection
   Array.prototype.sample = function(){
     return this[Math.floor(Math.random() * this.length)];
+  }
+
+
+
+
+// ****String prototype methods****
+
+
+  // #Pop : Returns a string with the last character removed
+  String.prototype.pop = function(){
+    return this.slice(0, this.length - 1);
   }
