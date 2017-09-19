a1fef4c4087424b1c6aed5dd8496d561f8298538
Gabriel Osborne <osbornegabriel@gmail.com>
2017-09-19 17:09:02 -0500

    Write methodize function, which takes a function and converts it to a prototype method

--- useful_methods.js
+++ useful_methods.js
@@ -1,3 +1,13 @@
+// ****General methods****
+  // #Methodize : attaches an existing function for use with a prototype
+  function methodize(func){
+    // var this = that;
+    return function(y){
+    return func(this, y);
+      };
+  }
+
+
 // ****Array prototype methods****
 
   // #Sample : Returns a random value from the array's collection
